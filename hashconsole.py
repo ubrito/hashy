eb268cdcf7a5e1e9df8a03c1b7dfd8daa3e140cc
ubrito <unaldo.brito@gmail.com>
2020-04-01 18:10:43 -0300

    Mais alguns ajustes

--- hashconsole.py
+++ hashconsole.py
@@ -2,57 +2,64 @@ import hashlib
 import os
 
 BLOCKSIZE = 65536
-funcoes = [ x for x in hashlib.algorithms_guaranteed]
 
-arquivo = input("Informe o caminho do arquivo que deseja obter o hash:\n  >>> ")
+def calcula_hash(arquivo, funcao):
 
-while not os.path.isfile(arquivo):
-    arquivo = input("\nArquivo não encontrado no caminho digitado!!\nInforme o caminho do arquivo que deseja obter o hash:\n  >>> ")
+    hash = hashlib.new(funcao)
 
-print("\nEscolha a função de hash:\n")
-
-for i in range(len(funcoes)):
-    print("{}\t-\t{}".format(i+1, funcoes[i]))
-
-funcao = int(input("\n  >>> "))
-
-hash = hashlib.new(funcoes[funcao-1])
-
-
-try:
-    with open(arquivo, 'rb') as arq:
-        buf = arq.read(BLOCKSIZE)
-        while len(buf) > 0:
-            hash.update(buf)
+    try:
+        with open(arquivo, 'rb') as arq:
             buf = arq.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hash.update(buf)
+                buf = arq.read(BLOCKSIZE)
 
-except:
-    print("Houve algum problema!")
+    except:
+        print("Houve algum problema!")
+
+    return hash.hexdigest(), hash.digest_size, hash.block_size
 
 
-hash_final = hash.hexdigest()
+def main():
 
-if len(hash_final) > len(arquivo):
-    tamanho = len(hash_final) + 4
-else:
-    tamanho = len(arquivo) + 4
+    funcoes = [ x for x in hashlib.algorithms_guaranteed ]
+    arquivo = input("Informe o caminho do arquivo que deseja obter o hash:\n  >>> ")
+    while not os.path.isfile(arquivo):
+        arquivo = input("\nArquivo não encontrado no caminho digitado!!\nInforme o caminho do arquivo que deseja obter o hash:\n  >>> ")
 
-print("")
-print("".center(tamanho,"="))
-print("ARQUIVO".center(tamanho))
-print("".center(tamanho,"-"))
-print(arquivo.center(tamanho))
-print("".center(tamanho,"="))
-print("FUNÇÃO".center(int(tamanho/3)), end="")
-print("DIGEST SIZE".center(int(tamanho/3)), end="")
-print("BLOCK SIZE".center(int(tamanho/3)))
-print("".center(tamanho,"-"))
-print(funcoes[funcao-1].center(int(tamanho/3)), end="")
-print(str(hash.digest_size).center(int(tamanho/3)), end="")
-print(str(hash.block_size).center(int(tamanho/3)))
-print("".center(tamanho,"="))
-print("HASH".center(tamanho))
-print("".center(tamanho,"-"))
-print(hash_final.center(tamanho))
-print("".center(tamanho,"="))
-print("")
+    print("\nEscolha a função de hash:\n")
+
+    for i in range(len(funcoes)):
+        print("{}\t-\t{}".format(i+1, funcoes[i]))
+
+    funcao = int(input("\n  >>> "))
+
+    hash_final, ds, bs = calcula_hash(arquivo, funcoes[funcao-1])
+
+    if len(hash_final) > len(arquivo):
+        tamanho = len(hash_final) + 4
+    else:
+        tamanho = len(arquivo) + 4
+
+    print("")
+    print("".center(tamanho,"="))
+    print("ARQUIVO".center(tamanho))
+    print("".center(tamanho,"-"))
+    print(arquivo.center(tamanho))
+    print("".center(tamanho,"="))
+    print("FUNÇÃO".center(int(tamanho/3)), end="")
+    print("DIGEST SIZE".center(int(tamanho/3)), end="")
+    print("BLOCK SIZE".center(int(tamanho/3)))
+    print("".center(tamanho,"-"))
+    print(funcoes[funcao-1].center(int(tamanho/3)), end="")
+    print(str(ds).center(int(tamanho/3)), end="")
+    print(str(bs).center(int(tamanho/3)))
+    print("".center(tamanho,"="))
+    print("HASH".center(tamanho))
+    print("".center(tamanho,"-"))
+    print(hash_final.center(tamanho))
+    print("".center(tamanho,"="))
+    print("")
+
+if __name__ == '__main__':
+    main()
